8cdb83e4626256a2f4d745ba8d8ed1dd7014f066
Levin Schmid <levin.schmid@mein.gmx>
2024-10-01 10:34:58 +0200

    finish 09

---
+++ 09-Quersumme/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Quersumme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Zahl = Console.ReadLine();
+            if (int.TryParse(Zahl, out int num) == true)
+            {
+                
+                
+                num = BerechneQuersumme(num);
+                Console.WriteLine(num);
+            }
+            else
+            {
+                Console.WriteLine("enter a Valide number");
+                
+            }Console.ReadKey();
+        }
+        static int BerechneQuersumme(int zahl)
+        {
+            int sum = 0;
+            while (true)
+            {
+                if (zahl != 0)
+                {
+                    sum = sum + (zahl % 10);
+                    zahl = zahl / 10;
+
+                    // todo: Quersumme von 'zahl' berechnen und zurückgeben (return)
+                }
+                else { 
+                break;
+                }
+            }return sum;
+        }
+    }
+}
