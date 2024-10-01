ce079937cd444e9c806453a35d36e1995bcc4540
Levin Schmid <levin.schmid@mein.gmx>
2024-10-01 13:08:46 +0200

    finish 12

---
+++ 12-Schleifen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Schleifen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            doSomething();
+            while (true)
+            {
+                doSomething();
+            }
+
+        }
+    }
+}
